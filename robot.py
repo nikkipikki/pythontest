fdb0c3f0516866c6dd6ceb44502446ddb88f44a9
nikkipikki <nicole.pehar@gmail.com>
2017-09-26 14:25:35 +0200

    Python Class

--- robot.py
+++ robot.py
@@ -4,8 +4,15 @@ robot_tax = 1.25
 book_price = 100
 book_tax = 1.06
 
-robot = {"price": 900, "tax": 1.25}
+class Product:
+	price = 0
+	tax = 1.25
 
-book = {"price": 100,"tax": 1.06}
+robot = Product()
+robot.price = 900
 
-print (robot["price"] * 2 * robot["tax"] + book["price"] * book["tax"])
+book = Product()
+book.price = 100 
+book.tax = 1.06
+
+print(robot.price * 2 * robot.tax + book.price * book.tax)
